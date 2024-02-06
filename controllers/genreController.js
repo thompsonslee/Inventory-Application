bc86c16aea1de9fc68e23878dc5348f0f0f51288
thompsonslee <thompson.slee@hotmail.co.uk>
2024-02-07 01:43:09 +1100

    add all controller and views to genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,35 +1,120 @@
-const genre = require("../models/genre")
-
+const Genre = require("../models/genre")
+const Album = require("../models/album")
+const { body, validationResult } = require("express-validator")
 const asyncHandler = require("express-async-handler")
 
 exports.genre_list = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
+    const genres = await Genre.find({}).sort({title: 1}).exec();
+
+    res.render("genre_list",{
+        title: "Genres",
+        genres: genres
+    })
 })
 
 exports.genre_create_get = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
+    res.render("genre_form",{
+        title: "Add Genre"
+    })
 })
 
-exports.genre_create_post = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
-})
+exports.genre_create_post =[
+    body("title","must not be empty")
+        .trim()
+        .isLength({min:1})
+        .escape(),
+    asyncHandler( async(req,res,next) => {
+        const errors = validationResult(req)
+
+        const genre = new Genre({
+            title: req.body.title
+        })
+        if(!errors.isEmpty()){
+            res.render("genre_form",{
+                title: "Add Genre",
+                genre: genre,
+                errors: errors
+            })
+            return
+        }
+        await genre.save()
+        res.redirect(genre.url)
+    })
+]
 
 exports.genre_delete_get = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
+    const [albums,genre] = await Promise.all([
+        Album.find({genre:req.params.id}),
+        Genre.findById(req.params.id)
+    ])
+
+    res.render("genre_delete",{
+        title:"Delete Genre",
+        genre: genre,
+        albums: albums
+    })
 })
 
 exports.genre_delete_post = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
+    const [albums,genre] = await Promise.all([
+        Album.find({genre:req.params.id}),
+        Genre.findById(req.params.id)
+    ])
+    if(albums.length){
+        res.render("genre_delete",{
+            title:"Delete Genre",
+            genre: genre,
+            albums: albums
+        })
+        return
+    }
+    await Genre.findByIdAndDelete(req.body.genreID)
+    res.redirect("/genres")
 })
 
 exports.genre_update_get = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
+    const genre = await Genre.findById(req.params.id)
+
+    res.render("genre_form",{
+        title: "Update Genre",
+        genre: genre
+    })
 })
 
-exports.genre_update_post = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
-})
+exports.genre_update_post = [
+    body("title", "must contain title")
+        .trim()
+        .isLength({min:1})
+        .escape(),
+    asyncHandler( async(req,res,next) => {
+        const errors = validationResult(req)
+
+        const genre = new Genre({
+            _id: req.params.id,
+            title: req.body.title
+        })
+        if(!errors.isEmpty()){
+            res.render("genre_form",{
+                title: "edit",
+                genre: genre,
+                errors: errors
+            })
+            return
+        }
+        const newGenre = await Genre.findByIdAndUpdate(req.params.id,genre,{})
+        res.redirect(newGenre.url)
+    })
+
+]
 
 exports.genre_detail = asyncHandler( async(req,res,next) => {
-    res.send("NOT IMPLEMENTED");
+    const [albums,genre] = await Promise.all([
+        Album.find({genre:req.params.id}).sort({title:1}).exec(),
+        Genre.findById(req.params.id)
+    ])
+    res.render("genre_detail",{
+        title: genre.title,
+        genre: genre,
+        albums: albums
+    })
 })
